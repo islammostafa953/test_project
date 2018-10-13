ca93ad73d4b98b82e31226a3199e9e6018553f48
unknown <islam.mostafa953@gmail.com>
2018-10-13 13:31:23 +0200

    islam1

---
+++ js/js.js
@@ -0,0 +1,91 @@
+
+function open_curtain1()
+{
+  $("#curtain1").animate({width:20},1000);
+ $("#curtain2").animate({width:20},1000);
+}
+/*
+ 
+function ph1() {
+$("#p1").animate({display:none},1000);
+
+ }
+*/
+
+
+function close_curtain1()
+{
+ $("#curtain1").animate({width:210},1000);
+ $("#curtain2").animate({width:200},1000);
+   
+ }
+ function ph1() {
+	  $("#p1").animate({opacity:0},1000);
+
+  }
+  function pv1() {
+	  	  $("#p1").animate({opacity:1},2000);
+
+  }
+  
+ 
+ 
+function open_curtain2()
+{
+ $("#curtain3").animate({width:20},1000);
+ $("#curtain4").animate({width:20},1000);
+}
+function close_curtain2()
+{
+ $("#curtain3").animate({width:210},1000);
+ $("#curtain4").animate({width:200},1000);}
+ 
+ function open_curtain3()
+{
+ $("#curtain5").animate({width:20},1000);
+ $("#curtain6").animate({width:20},1000);
+}
+function close_curtain3()
+{
+ $("#curtain5").animate({width:210},1000);
+ $("#curtain6").animate({width:200},1000);
+ }
+ 
+ function open_curtain4()
+{
+ $("#curtain7").animate({width:20},1000);
+ $("#curtain8").animate({width:20},1000);
+}
+function close_curtain4()
+{
+ $("#curtain7").animate({width:210},1000);
+ $("#curtain8").animate({width:200},1000);
+ }
+ 
+ function open_curtain5()
+{
+ $("#curtain9").animate({width:20},1000);
+ $("#curtain10").animate({width:20},1000);
+}
+function close_curtain5()
+{
+ $("#curtain9").animate({width:210},1000);
+ $("#curtain10").animate({width:200},1000);
+ }
+ 
+  function open_curtain6()
+{
+ $("#curtain11").animate({width:20},1000);
+ $("#curtain12").animate({width:20},1000);
+}
+function close_curtain6()
+{
+ $("#curtain11").animate({width:210},1000);
+ $("#curtain12").animate({width:200},1000);
+ }
+ 
+ 
+ 
+ 
+ 
+ 
